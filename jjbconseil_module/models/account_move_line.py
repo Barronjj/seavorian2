8cb3ab74b1b5d843071ed2b4ff3a7e8ab961bf4b
Vincent DUBREIL <vincent.dubreil@jjbconseil.com>
2022-09-09 12:59:48 +0000

    manifest2

--- jjbconseil_module/models/account_move_line.py
+++ jjbconseil_module/models/account_move_line.py
@@ -7,11 +7,17 @@ class AccountMoveLine(models.Model):
     
     @api.model
     def create(self, values):
-        record = super(AccountMoveLine, self).create(values)
+        if 'product_id' in values:
+            prd = values.get('product_id')
+            if record.prd.categ_id.s_analytic_account_default:
+                record['analytic_account_id'] = record.product_id.categ_id.s_analytic_account_default
         
-        if record:
-            # on cherche la catégorie de l'article, puis si elle a un compte analytique par défaut, on le met dans la ligne de facture
-            if record.product_id.categ_id.s_analytic_account_default:
-                record.analytic_account_id = record.product_id.categ_id.s_analytic_account_default
+        return super(AccountMoveLine, self).create(values)
+#         record = super(AccountMoveLine, self).create(values)
+        
+#         if record:
+#             # on cherche la catégorie de l'article, puis si elle a un compte analytique par défaut, on le met dans la ligne de facture
+#             if record.product_id.categ_id.s_analytic_account_default:
+#                 record['analytic_account_id'] = record.product_id.categ_id.s_analytic_account_default
                 
-        return record
+#         return record
