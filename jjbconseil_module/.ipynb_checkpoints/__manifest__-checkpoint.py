1d8c6ae1d1aeabf3a5c89758128172d90c5badb1
Vincent DUBREIL <vincent.dubreil@jjbconseil.com>
2022-09-22 14:12:26 +0000

    ajout depends dans manifest

--- jjbconseil_module/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ jjbconseil_module/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','product','analytic','account_analytic_default','account_analytic_default_purchase','analytic_enterprise','account_accountant'],
+    'depends': ['base','account','product','analytic','account_analytic_default','account_analytic_default_purchase','analytic_enterprise','account_accountant','crm','sale'],
 
     # always loaded
     'data': [
